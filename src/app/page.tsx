9431661dd5c36802384b2b3ff8ab084235f35472
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-04-13 19:55:05 +0300

    add cursor and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home Page</h1>
-      <p className="font-montez">Montez</p>
+      <h1 className="text-xl text-secondary-green">Home Page</h1>
+      <div className="hover:cursor-custom">
+        <p className="font-montez">Montez</p>
+      </div>
       <p className="font-arsenal">Arsenal</p>
       <p className="font-serif">Lora</p>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
         serif: ['Lora', 'serif'],
         montez: ['Montez', 'cursive'],
       },
+      colors: {
+        primary: 'rgb(22, 22, 22);',
+        secondary: {
+          green: 'rgb(0, 133, 122);',
+        },
+      },
+      cursor: {
+        custom: 'url("/cursor.png"), auto', // Example custom cursor
+      },
     },
   },
   plugins: [],
