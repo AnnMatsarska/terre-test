c9759f69e5baf6ecd5afc711d33f085d722e1644
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-04-14 01:05:43 +0300

    add nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 
 const NavBar = () => {
-  return <div className="text-white">NavBar</div>;
+  return (
+    <div className="text-white">
+      <nav>
+        <ul className="font-arsenal text-xl font-normal leading-5 flex gap-[41px]">
+          <li className="hover:cursor-custom hover:underline">menu</li>
+          <li className="hover:cursor-custom hover:underline">book now</li>
+          <li className="hover:cursor-custom hover:underline">gift cards</li>
+          <li className="hover:cursor-custom">
+            <Image src="/flag.svg" alt="Logo" width={40} height={26} />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
